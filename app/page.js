6e172a3ecd30abf3551555614d2f4a3123885afe
theprakashkumar <prakashcodes@outlook.com>
2023-06-29 16:58:03 +0530

    done

--- app/page.js
+++ app/page.js
@@ -1,5 +1,39 @@
+"use client";
+import { useEffect, useState } from "react";
+import CourseSearch from "./components/CourseSearch";
+import LoadingPage from "./loading";
+import Courses from "./components/Courses";
+
 const HomePage = () => {
-  return <div>Home Page</div>;
+  const [courses, setCourses] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchCourses = async () => {
+      const response = await fetch("http://localhost:3000/api/courses");
+      const data = await response.json();
+      setCourses(data);
+      setLoading(false);
+    };
+
+    fetchCourses();
+  }, []);
+
+  if (loading) {
+    return <LoadingPage />;
+  }
+
+  const getSearchResults = (courses) => {
+    setCourses(courses);
+  };
+
+  return (
+    <>
+      <h1>Traversy Media</h1>
+      <CourseSearch getSearchResults={getSearchResults} />
+      <Courses courses={courses} />
+    </>
+  );
 };
 
 export default HomePage;
